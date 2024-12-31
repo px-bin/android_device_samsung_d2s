6e0ec71845a3562b701ea4f8cadfe6cf10c17a28
pedro <pxd.bin@gmail.com>
2024-12-31 18:08:11 -0300

    crdroid 15 bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The aospOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_d2s.mk
+    $(LOCAL_DIR)/crdroid_d2s.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_d2s-eng \
-    aosp_d2s-user \
-    aosp_d2s-userdebug
+    crdroid_d2s-eng \
+    crdroid_d2s-user \
+    crdroid_d2s-userdebug
--- crdroid_d2s.mk
+++ crdroid_d2s.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The aospOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -36,26 +36,24 @@ TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
 
-## Inherit some common aosp stuff
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+## Inherit some common crDroid stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Project-Elixir
+# crDroid Stuff
 IS_PHONE := true
 TARGET_SUPPORTS_GOOGLE_RECORDER := false
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BUILD_APERTURE_CAMERA := true
-FORCE_LAWNCHAIR := true
-ELIXIR_BUILD_TYPE := OFFICIAL
-BUILD_USERNAME := FrEeRuNnEr4EvEr
-BUILD_HOSTNAME := Elixir
+BUILD_USERNAME := PX-0
+BUILD_HOSTNAME := crDroid
 TARGET_INCLUDE_STOCK_ACORE := false
 EXTRA_UDFPS_ANIMATIONS := true
 TARGET_BOOT_ANIMATION_RES := 1440
 
 ## Device identifier, this must come after all inclusions
-PRODUCT_NAME := aosp_d2s
+PRODUCT_NAME := lineage_d2s
 PRODUCT_DEVICE := d2s
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-N975F
